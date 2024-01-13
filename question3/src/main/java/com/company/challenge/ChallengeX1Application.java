cc3d58fc97b704808df7e6cd665e107f9e3c475a
José Kleiton da Silva <jose.kleiton.ext@dasa.com.br>
2024-01-13 12:49:49 -0300

    structure of question 3

---
+++ question3/src/main/java/com/company/challenge/ChallengeX1Application.java
@@ -0,0 +1,13 @@
+package com.company.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeX1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeX1Application.class, args);
+	}
+
+}
---
+++ question3/src/test/java/com/company/challenge/ChallengeX1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeX1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
